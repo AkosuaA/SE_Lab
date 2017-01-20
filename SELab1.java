43c8f4e8c5d27ed45ed29afbf86beecc47349a57
Deborah Attuah <Deborah Attuah>
2017-01-20 17:29:22 +0000

    First

---
+++ SELab1.java
@@ -0,0 +1,22 @@
+public class SELab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        if (args.length == 3) {
+            System.out.println("Enter your operation.");
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[2]);
+            String c = args[1];
+            int d = 0;
+            if (c.equals("+")) {
+                d = a + b;
+            }
+
+            System.out.println(d);
+        }
+    }
+
+}
